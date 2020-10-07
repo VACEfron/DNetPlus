13ddb3228417ec247b0097b23366db28155dce48
builderb gamer <builderbgamer@gmail.com>
2020-10-07 08:32:02 +0100

    Fix package and testing client.

--- DNetPlus/AssemblyInfo.cs
+++ DNetPlus/AssemblyInfo.cs
@@ -4,3 +4,4 @@
 [assembly: InternalsVisibleTo("Discord.Net.WebSocket")]
 [assembly: InternalsVisibleTo("Discord.Net.Webhook")]
 [assembly: InternalsVisibleTo("Discord.Net.Commands")]
+[assembly: InternalsVisibleTo("Discord.Net.Core")]
--- TestBot/Program.cs
+++ TestBot/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static DiscordShardedClient Client;
+        public static void Main(string[] args)
         {
-           
+            Start().GetAwaiter().GetResult();
+        }
+        public static async Task Start()
+        {
+            Client = new DiscordShardedClient();
+            string File = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/DiscordBots/Boaty/Config.json";
+            Client.Log += Client_Log;
+            await Client.LoginAsync(Discord.TokenType.Bot, JObject.Parse(System.IO.File.ReadAllText(File))["Discord"].ToString());
+            await Client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private static async Task Client_Log(Discord.LogMessage arg)
+        {
+            Console.WriteLine($"[{arg.Source}] {arg.Message}");
         }
     }
 }
