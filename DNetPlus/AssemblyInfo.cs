9e5d4b99f5061538db87ef316e54bddfc262fe32
builderb gamer <builderbgamer@gmail.com>
2020-10-07 05:56:02 +0100

    Make GetShardIdFor guildid public and fix package issues.

---
+++ DNetPlus/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Discord.Net.Rest")]
+[assembly: InternalsVisibleTo("Discord.Net.WebSocket")]
+[assembly: InternalsVisibleTo("Discord.Net.Webhook")]
+[assembly: InternalsVisibleTo("Discord.Net.Commands")]
