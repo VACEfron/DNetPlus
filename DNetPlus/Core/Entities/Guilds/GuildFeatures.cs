b9becf80271407bfde6d825bd40e1e1eb17c2e99
builderb gamer <builderbgamer@gmail.com>
2020-10-26 00:35:50 +0000

    Add guild features class and GetGuildFeatures method.

---
+++ DNetPlus/Core/Entities/Guilds/GuildFeatures.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNetPlus.Core.Entities.Guilds
+{
+    public class GuildFeatures
+    {
+        /// <summary>
+        /// 	Guild has access to set an invite splash background.
+        /// </summary>
+        public bool InviteSplash { get; internal set; }
+        /// <summary>
+        /// Guild has access to set 384kbps bitrate in voice. (previously VIP voice servers)
+        /// </summary>
+        public bool VipRegions { get; internal set; }
+        /// <summary>
+        /// 	Guild has access to set a vanity URL.
+        /// </summary>
+        public bool VanityUrl { get; internal set; }
+        /// <summary>
+        /// 	Guild is verified by Discord.
+        /// </summary>
+        public bool Verified { get; internal set; }
+        /// <summary>
+        /// Guild is partnered by Discord.
+        /// </summary>
+        public bool Partnered { get; internal set; }
+        /// <summary>
+        /// Guild has access to use commerce features. (i.e. create store channels)
+        /// </summary>
+        public bool Commerce { get; internal set; }
+        /// <summary>
+        /// Guild has access to create news channels.
+        /// </summary>
+        public bool News { get; internal set; }
+        /// <summary>
+        /// Guild is lurkable and able to be discovered in the directory.
+        /// </summary>
+        public bool Discoverable { get; internal set; }
+        /// <summary>
+        /// Guild is able to be featured in the directory.
+        /// </summary>
+        public bool Featurable { get; internal set; }
+        /// <summary>
+        /// Guild has access to set an animated guild icon.
+        /// </summary>
+        public bool AnimatedIcon { get; internal set; }
+        /// <summary>
+        /// Guild has access to set a guild banner image.
+        /// </summary>
+        public bool Banner { get; internal set; }
+        /// <summary>
+        /// Guild has enabled the welcome screen.
+        /// </summary>
+        public bool WelcomeScreen { get; internal set; }
+        /// <summary>
+        /// Guild can enable welcome screen and discovery, and receives community updates.
+        /// </summary>
+        public bool Community { get; internal set; }
+        /// <summary>
+        /// Guild is lurkable and able to be discovered in the directory.
+        /// </summary>
+        public bool DiscoverableOld { get; internal set; }
+    }
+}
--- DNetPlus/Core/Extensions/GuildExtensions.cs
+++ DNetPlus/Core/Extensions/GuildExtensions.cs
@@ -1,3 +1,5 @@
+using DNetPlus.Core.Entities.Guilds;
+
 namespace Discord
 {
     /// <summary>
@@ -20,5 +22,66 @@ namespace Discord
         /// <returns> A <c>bool</c> indicating if the guild boost messages are enabled in the system channel. </returns>
         public static bool GetGuildBoostMessagesEnabled(this IGuild guild)
             => !guild.SystemChannelFlags.HasFlag(SystemChannelMessageDeny.GuildBoost);
+
+        /// <summary>
+        ///    Gets a guilds feature set.
+        /// </summary>
+        /// <param name="guild"> The guild to check. </param>
+        /// <returns> Returns a <see cref="GuildFeatures"/> that has a bool indicating if any features are available for the guild. </returns>
+        public static GuildFeatures GetGuildFeatures(this IGuild guild)
+        {
+            GuildFeatures features = new GuildFeatures();
+            if (guild.Features.Count == 0)
+                return features;
+            foreach(string s in guild.Features)
+            {
+                switch (s)
+                {
+                    case "INVITE_SPLASH":
+                        features.InviteSplash = true;
+                        break;
+                    case "VIP_REGIONS":
+                        features.VipRegions = true;
+                        break;
+                    case "VANITY_URL":
+                        features.VanityUrl = true;
+                        break;
+                    case "VERIFIED":
+                        features.Verified = true;
+                        break;
+                    case "PARTNERED":
+                        features.Partnered = true;
+                        break;
+                    case "COMMERCE":
+                        features.Commerce = true;
+                        break;
+                    case "NEWS":
+                        features.News = true;
+                        break;
+                    case "DISCOVERABLE":
+                        features.Discoverable = true;
+                        break;
+                    case "FEATURABLE":
+                        features.Featurable = true;
+                        break;
+                    case "ANIMATED_ICON":
+                        features.AnimatedIcon = true;
+                        break;
+                    case "BANNER":
+                        features.Banner = true;
+                        break;
+                    case "WELCOME_SCREEN_ENABLED":
+                        features.WelcomeScreen = true;
+                        break;
+                    case "COMMUNITY":
+                        features.Community = true;
+                        break;
+                    case "ENABLED_DISCOVERABLE_BEFORE":
+                        features.DiscoverableOld = true;
+                        break;
+                }
+            }
+            return features;
+        }
     }
 }
